4a3df4e84bc19f5afa5d3f3dee0c8cd8e6a26ed2
FanaticoCoder <renatofrancisco242006@gmail.com>
2026-06-03 12:32:07 -0300

    update

--- main.js
+++ main.js
@@ -4,4 +4,9 @@ var button1 = document.createElement('div');
 button1.className = 'button1';
 document.body.appendChild(button1);
 button1.textContent = 'PLAY'
+
+button1.addEventListener('touchstart', ()=>{
+  button1.remove();
+});
 // TELA DE JOGO
+
