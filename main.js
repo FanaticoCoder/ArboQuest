6d330b5bd5936f3cb3de8c3e43b5831224b48785
FanaticoCoder <renatofrancisco242006@gmail.com>
2026-06-03 12:20:52 -0300

    update

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log('Hello World!');
+// TELA INICIAL
+
+var button1 = document.createElement('div');
+button1.className = 'button1';
+document.body.appendChild(button1);
+button1.textContent = 'PLAY'
+// TELA DE JOGO
